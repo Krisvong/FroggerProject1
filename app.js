c11cae1827b4e33e417660bf1d35783e80f850e5
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-01-17 13:32:30 -0500

    added classes to logs and cars with background color

--- app.js
+++ app.js
@@ -2,25 +2,30 @@ const timeLeftDisplay = document.querySelector('#time-left');
 const resultDisplay = document.querySelector('#result');
 const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
-console.log(squares)
-
+console.log(squares);
 let currentIndex = 76;
+const width = 9;
 
 function moveFrog(e) {
-
+    squares[currentIndex].classList.remove('frog')
+    // console.log(e)
     switch(e.key) {
         case 'ArrowLeft' :
             console.log('move left')
+            if (currentIndex % width !== 0) currentIndex -= 1
             break
         case 'ArrowRight' :
             console.log('move right')
+            if (currentIndex % width < width - 1) currentIndex += 1
             break
         case 'ArrowUp' :
-                console.log('move up')
-                break
+            console.log('move up')
+            if (currentIndex - width >=0 ) currentIndex -= width
+            break
         case 'ArrowDown' :
-                console.log('move down')
-                break            
+            console.log('move down')
+            if ( currentIndex + width < width * width) currentIndex += width
+            break            
     }
     
     squares[currentIndex].classList.add('frog')
