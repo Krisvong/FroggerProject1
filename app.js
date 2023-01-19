a3c9c3a1c33fb1a3cd463d75b8266ebbd14ffbd5
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-01-19 12:51:01 -0500

    added frog image

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const timeLeftDisplay = document.querySelector('#time-left');
-const resultDisplay = document.querySelector('#result');
-const startPauseButton = document.querySelector('#start-pause-button');
-const squares = document.querySelectorAll('.grid div');
-const logsLeft = document.querySelectorAll('.log-left');
-const logsRight = document.querySelectorAll('.log-right');
-const carsLeft = document.querySelectorAll('.car-left');
-const carsRight = document.querySelectorAll('.car-right');
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+const logsLeft = document.querySelectorAll('.log-left')
+const logsRight = document.querySelectorAll('.log-right')
+const carsLeft = document.querySelectorAll('.car-left')
+const carsRight = document.querySelectorAll('.car-right')
 
-console.log(squares);
 
-let currentIndex = 76;
-const width = 9;
-
+console.log(squares)
+let currentIndex = 76
+const width = 9
+let timerId
+let outcomeTimerId
+let currentTime = 20
+ 
+//move the frog
 function moveFrog(e) {
     squares[currentIndex].classList.remove('frog')
     // console.log(e)
@@ -19,7 +23,7 @@ function moveFrog(e) {
     switch(e.key) {
         case 'ArrowLeft' :
             // console.log('move left')
-            if (currentIndex % width !== 0) currentIndex -= 1
+            if (currentIndex % width !== 0) currentIndex -= 1;
             break
         case 'ArrowRight' :
             // console.log('move right')
@@ -35,18 +39,23 @@ function moveFrog(e) {
             break            
     }
     
-    squares[currentIndex].classList.add('frog');
+    squares[currentIndex].classList.add('frog')
 
 }
 
-document.addEventListener('keyup', moveFrog);
-
-
 function autoMoveElements() {
-    logsLeft.forEach(logLeft => moveLogLeft(logLeft));
-    logsRight.forEach(logRight => moveLogRight(logRight));
-    carsLeft.forEach(carLeft => moveCarLeft(carLeft));
-    carsRight.forEach(carRight => moveCarRight(carRight));
+    currentTime--
+    timeLeftDisplay.textContent = currentTime
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+    logsRight.forEach(logRight => moveLogRight(logRight))
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRight(carRight))
+    
+}
+
+function checkOutComes() {
+    lose()
+    win()
 }
 
 
@@ -133,4 +142,42 @@ function moveCarRight(carRight) {
             break         
     }
 }
-setInterval(autoMoveElements, 1000)
+
+function lose() {
+    if (
+        squares[currentIndex].classList.contains('c1') ||
+        squares[currentIndex].classList.contains('l4') ||
+        squares[currentIndex].classList.contains('l5') ||
+        currentTime <= 0
+    ) {
+        resultDisplay.textContent = 'You lose!'
+        clearInterval(timerId)
+        clearInterval(outcomeTimerId)
+        squares[currentIndex].classList.remove('frog')
+        document.removeEventListener('keyup', moveFrog)
+    }
+}
+
+function win() {
+    if(squares[currentIndex].classList.contains('ending-block')) {
+        resultDisplay.textContent = 'You Win!'
+        clearInterval(timerId)
+        clearInterval(outcomeTimerId)
+        document.removeEventListener('keyup', moveFrog)
+    }
+}
+
+startPauseButton.addEventListener('click', () => {
+    if(timerId) {
+        clearInterval(timerId)
+        clearInterval(outcomeTimerId)
+        outcomeTimerId = null
+        timerId = null
+        document.removeEventListener('keyup', moveFrog)
+    } else {
+        timerId = setInterval(autoMoveElements, 1000)
+        outcomeTimerId = setInterval(checkOutComes, 50)
+        document.addEventListener('keyup', moveFrog)
+    }
+})
+
